e5544f09208fc9fe8f107f08caf4e449ef7dfec1
IMAN-ITCompany <107607551+IMAN-ITCompany@users.noreply.github.com>
2022-06-23 15:28:51 +0500

    update

--- account/admin.py
+++ account/admin.py
@@ -2,19 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from account.models import Customuser, Region, Passport
+from account.models import Customuser, Region
 
 
 class CustomuserAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name']
 
-class RegionAdmin(admin.ModelAdmin):
-    list_display = ['id','name_uz','name_ru','name_en']
 
-class PassportAdmin(admin.ModelAdmin):
-    list_display = ['id']
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_uz', 'name_ru', 'name_en']
 
 
 admin.site.register(Customuser, CustomuserAdmin)
 admin.site.register(Region, RegionAdmin)
-admin.site.register(Passport, PassportAdmin)
